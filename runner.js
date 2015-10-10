381be32bdda6c2151ae0c79f35738109ba96daf5
elsehow <yes@cosmopol.is>
2015-10-10 15:33:32 -0700

    problems and solutions

--- runner.js
+++ runner.js
@@ -3,7 +3,8 @@
 var adventure = require('adventure');
 var shop = adventure('example-adventure');
 
-var problems = [ 'maptrap', 'primetime', 'rookie-cookie', 'cookie-farm' ];
+// var problems = [ 'maptrap', 'primetime', 'rookie-cookie', 'cookie-farm' ];
+var problems = [ 'cookied']
 problems.forEach(function (prob) {
     shop.add(prob, function () { return require('./problems/' + prob) });
 });
